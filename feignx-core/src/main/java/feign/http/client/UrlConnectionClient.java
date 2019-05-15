4ace413667a0c83c20ba072fcfd270d9bc531b90
Kevin Davis <kdavisk6@gmail.com>
2019-05-15 11:19:34 -0400

    Use Apache 2.0 License (#4)
    * Configuring License Plugin
    * Adding License Notices to Source Files

--- feignx-core/src/main/java/feign/http/client/UrlConnectionClient.java
+++ feignx-core/src/main/java/feign/http/client/UrlConnectionClient.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 OpenFeign Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package feign.http.client;
 
 import feign.Client;
@@ -27,7 +43,7 @@ public class UrlConnectionClient implements Client {
    *
    * @param request to send.
    * @return Response received.
-   * @throws HttpException if any error occurred during processing.
+   * @throws FeignException if any error occurred during processing.
    */
   @Override
   public Response request(Request request) throws FeignException {
