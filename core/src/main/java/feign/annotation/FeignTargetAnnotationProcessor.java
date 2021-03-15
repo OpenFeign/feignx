a43a3269d3c17e75ee89545aa57cab036d1d706e
Kevin Davis <kdavisk6@gmail.com>
2021-03-15 16:58:17 -0400

    GH-57: Adding Annotation Processor Skeletion
    This change introduces the skeleton for our Annotation Processor,
    that will allow users to choose between our default runtime target
    generator and a compile-time alternative.

---
+++ core/src/main/java/feign/annotation/FeignTargetAnnotationProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019-2021 OpenFeign Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package feign.annotation;
+
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
+/**
+ * Annotation Processor for {@link feign.FeignTarget} annotated interfaces.
+ */
+@SupportedAnnotationTypes({"feign.FeignTarget"})
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
+public class FeignTargetAnnotationProcessor extends AbstractProcessor {
+
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    return false;
+  }
+
+}
